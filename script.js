ca48c24e559f37417228d120d6a865dcca7b750f
Tarek <kheirtarek@hotmail.fr>
2022-04-14 12:26:09 +0200

    add changes

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 // Display close Holberton Overlay
 function on_holberton() {
-    document.getElementById("overlay-holberton").style.display = "block";
+  document.getElementById("overlay-holberton").style.display = "block";
 }
 function off_holberton() {
-    document.getElementById("overlay-holberton").style.display = "none";
+  document.getElementById("overlay-holberton").style.display = "none";
 }
 
 // Display close Upec Overlay
 function on_upec() {
-    document.getElementById("overlay-upec").style.display = "block";
+  document.getElementById("overlay-upec").style.display = "block";
 }
 function off_upec() {
-    document.getElementById("overlay-upec").style.display = "none";
+  document.getElementById("overlay-upec").style.display = "none";
 }
 
 // Display close Champlain Overlay
 function on_champlain() {
-    document.getElementById("overlay-champlain").style.display = "block";
+  document.getElementById("overlay-champlain").style.display = "block";
 }
 function off_champlain() {
-    document.getElementById("overlay-champlain").style.display = "none";
+  document.getElementById("overlay-champlain").style.display = "none";
 }
 
 
 // Copy paste function for email
 function copyText() {
-    console.log("enter");
-    let textToCopy = document.getElementById('mailToCopy');
-    let textArea = document.createElement("textarea");
-    textArea.value = textToCopy.textContent;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand("Copy");
-    textArea.remove();
-    document.getElementById('check').style.display = "block";
+  console.log("enter");
+  let textToCopy = document.getElementById('mailToCopy');
+  let textArea = document.createElement("textarea");
+  textArea.value = textToCopy.textContent;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("Copy");
+  textArea.remove();
+  document.getElementById('check').style.display = "block";
 }
